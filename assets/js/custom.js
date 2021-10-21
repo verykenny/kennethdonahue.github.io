02cc5e3af3e2f6aa8616ed0c93d8de3d168126bf
verykenny <16845292+verykenny@users.noreply.github.com>
2021-10-21 15:23:41 -0600

    added effect to showing and hiding modal

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -3,13 +3,16 @@ const skills = document.querySelectorAll('#skills .item');
 for (let skill of skills) {
 
     skill.addEventListener('click', (e) => {
-        skillModal = skill.nextElementSibling
-        skillModal.classList.remove('hidden')
+        skillModal = skill.nextElementSibling;
+        skillModal.style.visibility = 'visible';
+        skillModal.style.opacity = 1;
 
         skillModal.addEventListener('click', (e) => {
-            e.stopPropagation()
+            e.stopPropagation();
             if (e.target === skillModal) {
-                skillModal.classList.add('hidden')
+                skillModal.style.opacity = 0;
+                setTimeout(() => skillModal.style.visibility = 'hidden', 500)
+
             }
         })
     })
