55881de584923502d74ea48042378c40da3f43a1
verykenny <16845292+verykenny@users.noreply.github.com>
2021-10-22 09:52:06 -0600

    added info to project modals, removed skill modals"

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,22 +1,22 @@
-const skills = document.querySelectorAll('#skills .item');
+// const skills = document.querySelectorAll('#skills .item');
 
-for (let skill of skills) {
+// for (let skill of skills) {
 
-    skill.addEventListener('click', (e) => {
-        skillModal = skill.nextElementSibling;
-        skillModal.style.visibility = 'visible';
-        skillModal.style.opacity = 1;
+//     skill.addEventListener('click', (e) => {
+//         skillModal = skill.nextElementSibling;
+//         skillModal.style.visibility = 'visible';
+//         skillModal.style.opacity = 1;
 
-        skillModal.addEventListener('click', (e) => {
-            e.stopPropagation();
-            if (e.target === skillModal) {
-                skillModal.style.opacity = 0;
-                setTimeout(() => skillModal.style.visibility = 'hidden', 300)
+//         skillModal.addEventListener('click', (e) => {
+//             e.stopPropagation();
+//             if (e.target === skillModal) {
+//                 skillModal.style.opacity = 0;
+//                 setTimeout(() => skillModal.style.visibility = 'hidden', 300)
 
-            }
-        })
-    })
-}
+//             }
+//         })
+//     })
+// }
 
 
 const sites = document.querySelectorAll('#portfolio .item .image');
